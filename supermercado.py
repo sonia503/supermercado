f1778a797abf2f8926afd2881da8ae29df25be90
cote503 <profesonia2@hotmail.com>
2020-08-10 22:46:26 -0300

    supermercado.py

---
+++ supermercado.py
@@ -0,0 +1,12 @@
+class producto:
+	def __init(self,nombre,precio):
+		self.__nombre=nombre
+		self.__precio=precio
+		self.__primera_necesidad=primera_necesidad
+
+	def get_nombre(self):
+		return self.__nombre
+	def get primera_necesidad(self):
+		return self.__primera_necesidad
+	def precio(self):
+		
